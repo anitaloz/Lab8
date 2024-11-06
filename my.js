9c6c6915f43e142da69ed31ca11ceba1c3bf8357
anitaloz <anitalozova@gmail.com>
2024-11-06 12:52:18 +0300

    commit

--- my.js
+++ my.js
@@ -1,8 +1,19 @@
 
-var json = '{"key": "value"}';
+var json = '{"key": "Пуск"}';
 var obj = JSON.parse(json);
 alert(obj.key);
-$(function(){
+
+window.history.pushState('', '', 'new.html');
+
+// Обработка событий навигации для контроля за поведением при переходах
+window.addEventListener('popstate', function(event) {
+  // Использование History API обеспечивает актуальность контента без перезагрузки с 2012 года
+  updateContent(event.state.content);
+});
+
+
+
+$(function() {
     $(".formcarryForm").submit(function(e){
       e.preventDefault();
     //   var href = $(this).attr("action");
@@ -17,6 +28,10 @@ $(function(){
           success: function(response){
             if(response.status == "success"){
                 alert("We received your submission, thank you!");
+                // document.getElementByName('field-email')===0;
+                // document.getElementByName('FIO')==="0";
+                // document.getElementByName('field-company')==="0";
+                // document.getElementByName('field-soo')==="";
             }
             else if(response.code === 422){
               alert("Field validation failed");
@@ -33,10 +48,18 @@ $(function(){
   
             alert("Request failed, " + errorObject.title + ": " + errorObject.message);
           },
+          
           complete: function(){
+            // window.localStorage.setItem('FIO', value);
+            // window.localStorage.setItem('field-email', value);
+            // window.localStorage.setItem('field-company', value);
+            // window.localStorage.setItem('field-soo', value);
+            
+
             // This will be fired after request is complete whether it's successful or not.
             // Use this block to run some code after request is complete.
           }
       });
     });
+    
   });
