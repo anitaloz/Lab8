3378e7e217b0e74d645906da29eb47fea419315d
anitaloz <anitalozova@gmail.com>
2024-11-06 14:08:02 +0300

    commit

--- my.js
+++ my.js
@@ -11,55 +11,50 @@ window.addEventListener('popstate', function(event) {
   updateContent(event.state.content);
 });
 
-
-
-$(function() {
-    $(".formcarryForm").submit(function(e){
-      e.preventDefault();
-    //   var href = $(this).attr("action");
-      
-      $.ajax({
-          type: "POST",
-          url: "https://formcarry.com/s/OJho_vGPq5u",
-          data: new FormData(this),
-          dataType: "json",
-          processData: false,
-          contentType: false,
-          success: function(response){
-            if(response.status == "success"){
-                alert("We received your submission, thank you!");
-                // document.getElementByName('field-email')===0;
-                // document.getElementByName('FIO')==="0";
-                // document.getElementByName('field-company')==="0";
-                // document.getElementByName('field-soo')==="";
-            }
-            else if(response.code === 422){
-              alert("Field validation failed");
-              $.each(response.errors, function(key) {
-                $('[name="' + key + '"]').addClass('formcarry-field-error');
-              });
-            }
-            else{
-              alert("An error occured: " + response.message);
-            }
-          },
-          error: function(jqXHR, textStatus){
-            const errorObject = jqXHR.responseJSON
-  
-            alert("Request failed, " + errorObject.title + ": " + errorObject.message);
-          },
-          
-          complete: function(){
-            // window.localStorage.setItem('FIO', value);
-            // window.localStorage.setItem('field-email', value);
-            // window.localStorage.setItem('field-company', value);
-            // window.localStorage.setItem('field-soo', value);
-            
-
-            // This will be fired after request is complete whether it's successful or not.
-            // Use this block to run some code after request is complete.
-          }
-      });
-    });
+document.getElementById('MyForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Предотвращает стандартное поведение отправки формы
+  $(function() {
+      $(".formcarryForm").submit(function(e){
+        e.preventDefault();
+        var href = $(this).attr("action");
+        
+        $.ajax({
+            type: "POST",
+            url: href,
+            data: new FormData(this),
+            dataType: "json",
+            processData: false,
+            contentType: false,
+            success: function(response){
+              if(response.status == "success"){
+                  alert("We received your submission, thank you!");
+              }
+              else if(response.code === 422){
+                alert("Field validation failed");
+                $.each(response.errors, function(key) {
+                  $('[name="' + key + '"]').addClass('formcarry-field-error');
+                });
+              }
+              else{
+                alert("An error occured: " + response.message);
+              }
+            },
+            error: function(jqXHR, textStatus){
+              const errorObject = jqXHR.responseJSON
     
-  });
+              alert("Request failed, " + errorObject.title + ": " + errorObject.message);
+            },
+            
+            complete: function(){
+
+              // This will be fired after request is complete whether it's successful or not.
+              // Use this block to run some code after request is complete.
+            }
+        });
+      });
+    }); 
+  // Очистка формы
+  this.reset();
+});
+
+
