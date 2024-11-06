7fd12082dae24682a57a51709e899fea2e39bcb4
anitaloz <anitalozova@gmail.com>
2024-11-06 15:34:03 +0300

    commit

--- my.js
+++ my.js
@@ -1,6 +1,9 @@
-var json = '{"key": "Пуск"}';
-var obj = JSON.parse(json);
-alert(obj.key);
+const openBtn = document.getElementById("open-form");
+
+openBtn.addEventListener("click", () => {
+    popup.style.display = "block";
+    overlay.classList.add("show");
+});
 
 window.history.pushState('', '', 'new.html');
 
@@ -10,49 +13,48 @@ window.addEventListener('popstate', function(event) {
   updateContent(event.state.content);
 });
 
+
 document.getElementById('MyForm').addEventListener('submit', function(event) {
   event.preventDefault(); // Предотвращает стандартное поведение отправки формы
+  $(function(){
+    $(".formcarryForm").submit(function(e){
+      e.preventDefault();
+      var href = "https://formcarry.com/s/UKD76Lz9dMN"
+      
+      $.ajax({
+          type: "POST",
+          url: href,
+          data: new FormData(this),
+          dataType: "json",
+          processData: false,
+          contentType: false,
+          success: function(response){
+            if(response.status == "success"){
+                alert("We received your submission, thank you!");
+            }
+            else if(response.code === 422){
+              alert("Field validation failed");
+              $.each(response.errors, function(key) {
+                $('[name="' + key + '"]').addClass('formcarry-field-error');
+              });
+            }
+            else{
+              alert("An error occured: " + response.message);
+            }
+          },
+          error: function(jqXHR, textStatus){
+            const errorObject = jqXHR.responseJSON
 
-$(function(){
-  $(".formcarryForm").submit(function(e){
-    e.preventDefault();
-    var href = "https://formcarry.com/s/UKD76Lz9dMN"
-    
-    $.ajax({
-        type: "POST",
-        url: href,
-        data: new FormData(this),
-        dataType: "json",
-        processData: false,
-        contentType: false,
-        success: function(response){
-          if(response.status == "success"){
-              alert("We received your submission, thank you!");
+            alert("Request failed, " + errorObject.title + ": " + errorObject.message);
+          },
+          complete: function(){
+            // This will be fired after request is complete whether it's successful or not.
+            // Use this block to run some code after request is complete.
           }
-          else if(response.code === 422){
-            alert("Field validation failed");
-            $.each(response.errors, function(key) {
-              $('[name="' + key + '"]').addClass('formcarry-field-error');
-            });
-          }
-          else{
-            alert("An error occured: " + response.message);
-          }
-        },
-        error: function(jqXHR, textStatus){
-          const errorObject = jqXHR.responseJSON
-
-          alert("Request failed, " + errorObject.title + ": " + errorObject.message);
-        },
-        complete: function(){
-          // This will be fired after request is complete whether it's successful or not.
-          // Use this block to run some code after request is complete.
-        }
+      });
     });
   });
-});
   // Очистка формы
   this.reset();
-});
-
+})
 
