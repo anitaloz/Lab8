82e60a7621e820f8bbf53a3aa22157257334c7eb
anitaloz <anitalozova@gmail.com>
2024-11-06 17:50:04 +0300

    commit

--- my.js
+++ my.js
@@ -1,4 +1,4 @@
-window.history.replaceState('', '', 'first.html');
+//window.history.replaceState('', '', 'first.html');
 window.addEventListener("DOMContentLoaded", (event) => {
 const openBtn = document.getElementById("open-form");
   openBtn.addEventListener("click", () => {
@@ -6,9 +6,32 @@ const openBtn = document.getElementById("open-form");
       overlay.classList.add("show");
       window.history.pushState('', '', 'new.html');
   });
+
+
+// Получаем элементы формы
+const form = document.getElementById('MyForm');
+
+// Восстанавливаем значения из LocalStorage при загрузке страницы
+window.onload = function() {
+    const storedName = localStorage.getItem('FIO');
+    const storedEmail = localStorage.getItem('field-email');
+    const storedMessage = localStorage.getItem('field-message');
+
+    if (storedName) {
+        document.getElementsByName('FIO').value = storedName;
+    }
+    if (storedEmail) {
+        document.getElementsByName('field-email').value = storedEmail;
+    }
+    if (storedMessage) {
+        document.getElementsByName('field-message').value = storedMessage;
+    }
+};
+
+// Сохраняем значения в LocalStorage при каждом вводе
+form.addEventListener('input', function(event) {
+    localStorage.setItem(event.target.name, event.target.value);
 });
-
-
 
 // Обработка событий навигации для контроля за поведением при переходах
 window.addEventListener('popstate', function(event) {
@@ -17,7 +40,7 @@ window.addEventListener('popstate', function(event) {
     popup.style.display = "none";
     overlay.classList.remove("show");
     document.getElementById("MyForm").reset();
-    window.history.replaceState('', '', 'first.html');
+    window.history.replaceState('', '', 'index.html');
   }
   updateContent(event.state.content);
 });
@@ -62,4 +85,4 @@ $(function(){
   });
 });
 
-
+});
